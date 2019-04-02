eaeffbec31f7610ed931adbe98cfe3705302847c
Jonas Schulze <jonas.schulze@st.ovgu.de>
2019-04-02 18:44:22 +0200

    Add mvcs pull

--- pull.go
+++ pull.go
@@ -1,11 +1,40 @@
 package main
 
 import (
-	"errors"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sync"
 
 	"github.com/urfave/cli"
 )
 
 func pullRepositories(c *cli.Context) error {
-	return errors.New("not implemented")
+	config, err := readConfig()
+	if err != nil {
+		return err
+	}
+
+	var wg sync.WaitGroup
+	for _, remote := range config.Remotes {
+		wg.Add(len(remote.Repositories))
+		for _, repo := range remote.Repositories {
+			dir := filepath.Base(repo)
+			go func() {
+				pullRepository(config.VCS, dir)
+				wg.Done()
+			}()
+		}
+	}
+
+	wg.Wait()
+	return nil
+}
+
+func pullRepository(vcs, dir string) {
+	cmd := exec.Command(vcs, "pull")
+	cmd.Dir = dir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
